bea9e75110e454f33822a0a5e897372245378d15
Esteban Heredia <estebanhdg@gmail.com>
2023-01-24 09:55:23 -0300

    primera entrega react

--- src/Componentes/NavBar.jsx
+++ src/Componentes/NavBar.jsx
@@ -1,40 +1,58 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 
 function NavBar() {
+  const [state, setState] = useState({
+    tintas: false,
+    agujas: false,
+    descartables: false
+  });
 
-    const[toggleValue, setToggleValue] = useState(true);
+  function tintas() {
+    setState({...state, tintas: !state.tintas });
+  }
 
-    function toggle() {
-        setToggleValue(!toggleValue);
-    }
+  function agujas() {
+    setState({...state, agujas: !state.agujas });
+  }
 
+  function descartables() {
+    setState({...state, descartables: !state.descartables });
+  }
 
   return (
     <div className="container-main-navbar">
       <div className="container-sub-logo">
-        <img className="logo" src="../src/assets/img/logo.svg" alt="EH SUPPLIES Logo" />
+        <img
+          className="logo"
+          src="../src/assets/img/logo.svg"
+          alt="EH SUPPLIES Logo"
+        />
       </div>
       <div className="container-sub-button">
         <div className="container-button-div">
-          <button onClick={toggle}>Tintas</button>
-          <div className={toggle ? "algo" : "visible"}>
-          <div className="elementos">elemento1</div>
-          <div className="elementos">elemento2</div>
-          <div className="elementos">elemento3</div>
+          <button onClick={tintas}>Tintas</button>
+          <div className={state.tintas ? "show" : "hide"}>
+            <div className="elementos">elemento1</div>
+            <div className="elementos">elemento2</div>
+            <div className="elementos">elemento3</div>
           </div>
         </div>
         <div className="container-button-div">
-          <button>Agujas</button>
-          <div className="elementos">elemento1</div>
-          <div className="elementos">elemento2</div>
-          <div className="elementos">elemento3</div>
+          <button onClick={agujas}>Agujas</button>
+          <div className={state.agujas ? "show" : "hide"}>
+            <div className="elementos">elemento1</div>
+            <div className="elementos">elemento2</div>
+            <div className="elementos">elemento3</div>
+          </div>
         </div>
         <div className="container-button-div">
-          <button>Descartables</button>
-          <div className="elementos">elemento1</div>
-          <div className="elementos">elemento2</div>
-          <div className="elementos">elemento3</div>
+          <button onClick={descartables}>Descartables</button>
+          <div className={state.descartables ? "show" : "hide"}>
+            <div className="elementos">elemento1</div>
+            <div className="elementos">elemento2</div>
+            <div className="elementos">elemento3</div>
+          </div>
         </div>
       </div>
       <div>
